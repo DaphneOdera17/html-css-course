e5b8c227df35cb5617378a1f8e0440324e30e22b
Birdy <2944006086@qq.com>
2024-07-12 17:12:01 +0800

    finish the course

--- own/Omnifood-Optimizations/script.js
+++ own/Omnifood-Optimizations/script.js
@@ -1,24 +1,7 @@
-console.log("Hello world!");
-
-const myName = "Jonas Schmedtmann";
-const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
-
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
-
 ///////////////////////////////////////////////////////////
-// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
-
-///////////////////////////////////////////////////////////
-// Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
@@ -27,7 +10,6 @@ btnNavEl.addEventListener("click", function () {
 	headerEl.classList.toggle("nav-open");
 });
 
-///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
@@ -56,9 +38,6 @@ allLinks.forEach(function (link) {
 	});
 });
 
-///////////////////////////////////////////////////////////
-// Sticky navigation
-
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
@@ -78,12 +57,10 @@ const obs = new IntersectionObserver(
 		// In the viewport
 		root: null,
 		threshold: 0,
-		rootMargin: "-80px",
 	}
 );
 obs.observe(sectionHeroEl);
 
-///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
 	var flex = document.createElement("div");
